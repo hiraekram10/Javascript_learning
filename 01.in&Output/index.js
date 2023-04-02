3fcad7026f530f47d21f0485ee64db5091545fd4
Hira Ekram <heraiqram123@gmail.com>
2023-04-02 10:47:54 -0700

    taking input & giving output

--- 01.in&Output/index.js
+++ 01.in&Output/index.js
@@ -1,4 +1,14 @@
+// function submit(){
+//     let userinput = document.querySelector('input').value;
+//     console.log(userinput)
+// }
+
 function submit(){
-    let userinput = document.querySelector('input').value;
-    console.log(userinput)
+    let inputOne = document.querySelector('#inputOne').value;
+    let inputTwo = document.querySelector('#inputTwo').value;
+    let result = Number(inputOne) + Number(inputTwo)
+    console.log(result)
+    // console.log(inputOne,inputTwo)
+    let resultEle = document.querySelector('#resultElement')
+    resultEle.innerText = result;
 }
